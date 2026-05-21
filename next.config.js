49b6476e5cd24234e91bfad66fd5ff0bb89b2ad5
juan carlos cortes gamez <juancortescg111@gmail.com>
2026-05-21 14:25:20 -0500

    Fix: remove unsupported experimental and compiler config options from next config files

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: { reactCompiler: true },
-  experimental: { turbopack: false },
 };
 module.exports = nextConfig;
+
